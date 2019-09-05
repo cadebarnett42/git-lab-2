a45893eace946b8eee18ce414a28f01192ece462
Cade Barnett <cb346517@ohio.edu>
2019-09-05 16:09:35 -0400

    yur

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+//git-lab-program.cc
+
+/*
+ *        File: git-lab-program.cc
+ *      Author: Cade Barnett
+ *        Date: September 5th, 2019
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) 
+{
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
